9977c3c10acdd7a3753c02db1c9097c197a981c5
CodeAbsolute <68479079+CodeAbsolute@users.noreply.github.com>
2022-07-30 22:50:00 +0530

    final commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 const Order = require("../models/orderModel");
 const Product = require("../models/productModel");
-const ErrorHandler = require("../util/Errorhandler");
+const ErrorHandler = require("../util/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // Create new Order
